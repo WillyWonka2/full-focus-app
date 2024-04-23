e538af70b889d4a3f8aa92910abb617b3ddc69c9
Vladimir <vladimir.tsybon@gmail.com>
2024-04-23 09:10:55 +0300

    Update auth

--- app/components/screens/auth/Auth.tsx
+++ app/components/screens/auth/Auth.tsx
@@ -3,14 +3,17 @@ import Loader from '@/components/ui/layout/loader'
 import { useAuth } from '@/hooks/useAuth'
 import { IAuthFormData } from '@/types/auth.interface'
 import React, { FC, useState } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import {
 	View,
 	Text,
 	TouchableWithoutFeedback,
 	Keyboard,
-	Pressable
+	Pressable,
+	TextInput
 } from 'react-native'
+import cn from 'clsx'
+import { validEmail } from './email.rgx'
 
 const Auth: FC = () => {
 	const [isReq, setIsReq] = useState(false)
@@ -34,7 +37,7 @@ const Auth: FC = () => {
 		<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
 			<View className='items-center justify-center flex-1'>
 				<View className='w-3/4'>
-					<Text className='text-white text-4xl text-center font-bold'>
+					<Text className='text-white text-4xl text-center font-bold mb-5'>
 						{isReq ? 'Sign Up' : ' Sign in'}
 					</Text>
 					{isLoading ? (
@@ -42,7 +45,42 @@ const Auth: FC = () => {
 					) : (
 						<>
 							{/* Fields */}
-
+							<Controller
+								control={control}
+								name='email'
+								rules={{
+									required: 'Email is required',
+									pattern: {
+										value:  validEmail,
+										message: 'Your Email is invalid!'
+									}
+								}}
+								render={({
+									field: { value, onChange, onBlur },
+									fieldState: { error }
+								}) => (
+									<>
+										<View
+											className={cn(
+												'rounded bg-[#272541] border-solid border pb-3 pt-3 px-4 my-2',
+												!!error ? 'border-red-500' : 'border-trasnparent'
+											)}
+										>
+											<TextInput
+												placeholder='Enter email'
+												value={value}
+												onChangeText={onChange}
+												onBlur={onBlur}
+												autoCapitalize='none'
+												className='text-white text-lg leading-5'
+											/>
+										</View>
+										{error && (
+											<Text className='text-red-500'>{error.message}</Text>
+										)}
+									</>
+								)}
+							/>
 							<Button onPress={handleSubmit(onSubmit)}>Let's go</Button>
 							<Pressable
 								onPress={() => setIsReq(!isReq)}
