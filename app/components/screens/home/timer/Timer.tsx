61e3db057dc9985133016309fb986a2d6b1bad54
Vladimir <vladimir.tsybon@gmail.com>
2024-04-29 09:08:37 +0300

    update timer + start progress bar

--- app/components/screens/home/timer/Timer.tsx
+++ app/components/screens/home/timer/Timer.tsx
@@ -9,7 +9,44 @@ import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 const Timer: FC = () => {
 	const [isStarting, setIsStarting] = useState(false)
 	return (
-		<View>
+		<View className='justify-center items-center'>
+			<View className='self-center mt-20'>
+				<CountdownCircleTimer
+					isPlaying={isStarting}
+					duration={300}
+					colors={['#3A3570', '#664EF3']}
+					colorsTime={[7, 0]}
+					trailColor='#2F2F4C'
+					onComplete={() => setIsStarting(false)}
+					size={320}
+					strokeWidth={15}
+				>
+					{({ remainingTime }) => {
+						const hours = Math.floor(remainingTime / 3600)
+						const minutes = Math.floor((remainingTime % 3600) / 60)
+						const seconds = remainingTime % 60
+
+						if (hours == 0) {
+							return (
+								<Text className='color-white text-5xl'>{`${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}</Text>
+							)
+						}
+						return (
+							<Text className='color-white text-5xl'>{`${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}</Text>
+						)
+					}}
+				</CountdownCircleTimer>
+				<View className='mt-10 flex-row justify-center'>
+					{Array(7)
+						.fill(null)
+						.map((_, index) => (
+							<View className='flex-row items-center justify-center'>
+								<View className='w-4 h-4 bg-primary rounded-full' />
+								{index + 1 != 7 && <View className='w-5 h-0.5 bg-primary' />}
+							</View>
+						))}
+				</View>
+			</View>
 			<Pressable
 				className={cn(
 					'mt-10 self-center bg-primary w-16 h-16 items-center justify-center rounded-full',
@@ -24,14 +61,6 @@ const Timer: FC = () => {
 					size={38}
 				></Feather>
 			</Pressable>
-			<CountdownCircleTimer
-					isPlaying = {isStarting}
-					duration={7}
-					colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-					colorsTime={[7, 5, 2, 0]}
-				>
-					{({ remainingTime }) => <Text>{remainingTime}</Text>}
-				</CountdownCircleTimer>
 		</View>
 	)
 }
