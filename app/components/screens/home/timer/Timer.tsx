ec493608d9324b6a11fd95134893c3d139c9cffe
WillyWonka2 <vladimir.tsybon@gmail.com>
2024-05-01 10:07:34 +0300

    Fix and update Timer

--- app/components/screens/home/timer/Timer.tsx
+++ app/components/screens/home/timer/Timer.tsx
@@ -1,46 +1,57 @@
 import cn from 'clsx'
 import { Feather } from '@expo/vector-icons'
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import React from 'react'
 import { View, Text, Pressable } from 'react-native'
 import { AppConstant } from '@/app.const'
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 import { EnumStatus } from './timer.interface'
 
-const flowDuration = 1 * 10
-const sessionCount = 7
-const breakDuration = 1 * 20
+const flowDuration = 1 * 5
+const sessionCount = 5
+const breakDuration = 1 * 7
 // ToDo arrow next and prev
 
 const Timer: FC = () => {
 	const [isStarting, setIsStarting] = useState<true | false>(false)
 	const [status, setStatus] = useState<EnumStatus>(EnumStatus.WORK)
 	const [currentSession, setCurrentSession] = useState<number>(1)
+	const [duration, setDuration] = useState<number>(flowDuration)
+	const [key, setKey] = useState<number>(0)
+	const [isCompleted, setIsCompleted] = useState<boolean>(false)
 
-	function statusHandler(newStatus:string){
-		return newStatus == EnumStatus.WORK ? breakDuration : flowDuration
+	function statusHandler() {
+		return status == EnumStatus.WORK ? EnumStatus.REST : EnumStatus.WORK
 	}
+
+	function onComplete() {
+		if (currentSession == sessionCount) {
+			setIsCompleted(true)
+			setIsStarting(false)
+		} else {
+			setCurrentSession(prev => prev + 1)
+			setStatus(statusHandler())
+			setDuration(duration === breakDuration ? flowDuration : breakDuration)
+			setIsStarting(false)
+			setKey(prev => prev + 1)
+		}
+	}
+
 	return (
 		<View className='flex-1 justify-center'>
 			<View className='self-center'>
 				{/* TODO */}
 				<CountdownCircleTimer
+					key={key}
 					isPlaying={isStarting}
-					duration={flowDuration}
+					duration={duration}
 					colors={['#3A3570', '#664EF3']}
 					colorsTime={[
-						statusHandler(status),
+						status == EnumStatus.WORK ? flowDuration : breakDuration,
 						0
 					]}
 					trailColor='#2F2F4C'
-					onComplete={() => {
-						setCurrentSession(prev => prev + 1)
-						setStatus(status === EnumStatus.REST ? EnumStatus.WORK : EnumStatus.REST)
-						return {
-							shouldRepeat: true,
-							newInitialRemainingTime: statusHandler(status)
-						}
-					}}
+					onComplete={() => onComplete()}
 					size={320}
 					strokeWidth={15}
 				>
@@ -48,6 +59,12 @@ const Timer: FC = () => {
 						const hours = Math.floor(remainingTime / 3600)
 						const minutes = Math.floor((remainingTime % 3600) / 60)
 						const seconds = remainingTime % 60
+
+						if (isCompleted) {
+							return (
+								<Text className='color-white text-4xl font-bold'>SUCCESS!</Text>
+							)
+						}
 
 						if (hours === 0) {
 							return (
@@ -59,6 +76,7 @@ const Timer: FC = () => {
 								</>
 							)
 						}
+
 						return (
 							<>
 								<Text className='color-white text-5xl font-bold'>{`${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}</Text>
@@ -79,9 +97,11 @@ const Timer: FC = () => {
 							>
 								<View
 									className={cn('w-4 h-4 bg-[#2C2B3C] rounded-full', {
-										'bg-primary': index + 1 <= currentSession,
-										'w-6 h-6 bg-transparent border-4 border-indigo-500':
-											index === currentSession - 1
+										'bg-primary': index + 1 <= currentSession || isCompleted,
+										'bg-transparent':
+											index === currentSession - 1 && !isCompleted,
+										'w-6 h-6 border-4 border-primary':
+											index === currentSession - 1 && index === sessionCount - 1
 									})}
 								/>
 								{index + 1 != sessionCount && (
@@ -102,6 +122,7 @@ const Timer: FC = () => {
 				)}
 				onPress={() => setIsStarting(!isStarting)}
 				style={isStarting && AppConstant.shadow}
+				disabled={isCompleted}
 			>
 				<Feather
 					name={isStarting ? 'pause' : 'play'}
